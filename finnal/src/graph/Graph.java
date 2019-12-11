b912aedb019d2082bcb5870adf6cd875e7ae1eec
NotColdrain <53297467+NotColdrain@users.noreply.github.com>
2019-12-11 09:58:47 +0800

    调用graph.exe 程序的方法

---
+++ finnal/src/graph/Graph.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+//方法可以调用graph程序生成 "班级-借阅图书" 图表
+class Graph{
+    public static void run() throws IOException{
+        try{
+            Runtime.getRuntime().exec("graph.exe");
+        }
+        finally{
+            System.out.println("生成图表成功!");
+        }
+    }
+}
+
+/**
+ *
+class Main{
+    public static void main(String[] args) throws IOException{
+        Graph.run();
+    }
+}
+*/
